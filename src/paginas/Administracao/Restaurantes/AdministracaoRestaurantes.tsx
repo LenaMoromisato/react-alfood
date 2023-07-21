726a2e0b027a5ef47129a9606ab97faf3b9fd592
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-21 11:35:27 -0300

    Extraindo o layout base

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import IRestaurante from "../../../interfaces/IRestaurante"
-import { AppBar, Box, Button, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography, Link } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import http from "../../../http";
 
 import { Link as RouterLink } from "react-router-dom"
@@ -24,75 +24,40 @@ const AdministracaoRestaurantes = () => {
 
     return (
 
-        <>
-
-            <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar>
-                        <Typography variant="h6">
-                            Administração
-                        </Typography>
-                        <Box sx={{ display: 'flex', flexGrow: 1 }}>
-                            <Link component={RouterLink} to="/admin/restaurantes">
-                                <Button sx={{ my: 2, color: 'white' }}>
-                                    Restaurantes
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Nome
+                        </TableCell>
+                        <TableCell>
+                            Editar
+                        </TableCell>
+                        <TableCell>
+                            Excluir
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
+                        <TableRow>
+                            <TableCell>
+                                {restaurante.nome}
+                            </TableCell>
+                            <TableCell>
+                                [ <RouterLink to={`/admin/restaurantes/${restaurante.id}`}>editar</RouterLink> ]
+                            </TableCell>
+                            <TableCell>
+                                <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
+                                    Excluir
                                 </Button>
-                            </Link>
-                            <Link component={RouterLink} to="/admin/restaurantes/novo">
-                                <Button sx={{ my: 2, color: 'white' }}>
-                                    Novo Restaurante
-                                </Button>
-                            </Link>
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-
-            <Box>
-                <Container maxWidth="lg" sx={{ mt: 1 }}>
-                    <Paper sx={{ p: 2 }}>
-
-                        {/* conteúdo da página */}
-                        <TableContainer component={Paper}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>
-                                            Nome
-                                        </TableCell>
-                                        <TableCell>
-                                            Editar
-                                        </TableCell>
-                                        <TableCell>
-                                            Excluir
-                                        </TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
-                                        <TableRow>
-                                            <TableCell>
-                                                {restaurante.nome}
-                                            </TableCell>
-                                            <TableCell>
-                                                [ <RouterLink to={`/admin/restaurantes/${restaurante.id}`}>editar</RouterLink> ]
-                                            </TableCell>
-                                            <TableCell>
-                                                <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
-                                                    Excluir
-                                                </Button>
-                                            </TableCell>
-                                        </TableRow>
-                                    </TableRow>)}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-
-                    </Paper>
-                </Container>
-            </Box>
-
-        </>
+                            </TableCell>
+                        </TableRow>
+                    </TableRow>)}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,10 +1,9 @@
-import { AppBar, Box, Button, TextField, Typography, Container, Toolbar, Link, Paper } from "@mui/material"
+import { Box, Button, TextField, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import IRestaurante from "../../../interfaces/IRestaurante"
 import http from "../../../http"
 
-import { Link as RouterLink } from "react-router-dom"
 
 const FormularioRestaurante = () => {
 
@@ -42,58 +41,25 @@ const FormularioRestaurante = () => {
     }
 
     return (
-        <>
 
-            <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar>
-                        <Typography variant="h6">
-                            Administração
-                        </Typography>
-                        <Box sx={{ display: 'flex', flexGrow: 1 }}>
-                            <Link component={RouterLink} to="/admin/restaurantes">
-                                <Button sx={{ my: 2, color: 'white' }}>
-                                    Restaurantes
-                                </Button>
-                            </Link>
-                            <Link component={RouterLink} to="/admin/restaurantes/novo">
-                                <Button sx={{ my: 2, color: 'white' }}>
-                                    Novo Restaurante
-                                </Button>
-                            </Link>
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-
-            <Box>
-                <Container maxWidth="lg" sx={{ mt: 1 }}>
-                    <Paper sx={{ p: 2 }}>
-
-                        {/* conteúdo da página */}
-                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flexGrow: 1 }}>
-                            <Typography
-                                component="h1"
-                                variant="h6">
-                                Formulário de Restaurantes
-                            </Typography>
-                            <Box component="form" sx={{ width: '100%' }} onSubmit={aoSubmeterForm}>
-                                <TextField
-                                    value={nomeRestaurante}
-                                    onChange={evento => setNomeRestaurante(evento.target.value)}
-                                    label="Nome do Restaurante"
-                                    variant="standard"
-                                    fullWidth
-                                    required
-                                />
-                                <Button sx={{ marginTop: 1 }} type="submit" fullWidth variant="outlined">Salvar</Button>
-                            </Box>
-                        </Box>
-
-                    </Paper>
-                </Container>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flexGrow: 1 }}>
+            <Typography
+                component="h1"
+                variant="h6">
+                Formulário de Restaurantes
+            </Typography>
+            <Box component="form" sx={{ width: '100%' }} onSubmit={aoSubmeterForm}>
+                <TextField
+                    value={nomeRestaurante}
+                    onChange={evento => setNomeRestaurante(evento.target.value)}
+                    label="Nome do Restaurante"
+                    variant="standard"
+                    fullWidth
+                    required
+                />
+                <Button sx={{ marginTop: 1 }} type="submit" fullWidth variant="outlined">Salvar</Button>
             </Box>
-        </>
+        </Box>
     )
 }
 
